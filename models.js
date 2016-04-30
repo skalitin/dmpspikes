489821baa476012e1bc797362d6a9f97c2724812
Sergey Kalitin <sergey.kalitin@gmail.com>
2016-04-30 13:05:42 +0300

    Initial commit

---
+++ models.js
@@ -0,0 +1,28 @@
+var models = {
+    "object": {
+        "fields": {
+            "name": {
+                "type": "string"
+            },
+            "role": {
+                "type": "string"
+            },
+            "department": {
+                "type": "string"
+            },
+            "location": {
+                "type": "string"
+            },
+            "email": {
+                "type": "string"
+            },
+            "domain": {
+                "type": "string"
+            },
+            "domainController": {
+                "type": "string"
+            }
+        }
+    }
+}
+module.exports = models;
