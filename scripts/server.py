70d05b6c59241024846b14e2dca7b250f622e701
Sergey Kalitin <sergey.kalitin@gmail.com>
2016-05-02 10:53:56 +0300

    Recreate callbacks

--- scripts/server.py
+++ scripts/server.py
@@ -11,17 +11,18 @@ format = ' '.join(
      '%(message)s'])
 
 logging.basicConfig(format=format, level=logging.INFO)
-
+import requests
 import asyncio
 import aiohttp
 from aiohttp import web
 
+api = "http://localhost:8080/api"
 
 async def create_collection(request):
     response = await request.read()
     data = json.loads(response.decode())
 
-    if not (data["data"].__contains__("draft") & data["data"]["draft"]):
+    if not data["data"].get("draft"):
         collection = {
             "name": data["data"]["name"],
             "connectionId": data["id"],
@@ -29,7 +30,7 @@ async def create_collection(request):
         }
 
         logging.info("Creating collection %s..." % collection["name"])
-        response = await aiohttp.post("http://localhost:8080/api/collections", data=collection)
+        response = await aiohttp.post(api + "/collections", data=collection)
         logging.info(response)
 
     return web.Response(body=b"{}")
@@ -42,30 +43,40 @@ async def test_connection(request):
     connection_data = data["data"]
     connection_id = data["id"]
 
-    if connection_data.__contains__("draft") & connection_data["draft"]:
-        tenant_id = connection_data["tenantId"]
-        client_id = connection_data["clientId"]
-        client_secret = connection_data["clientSecret"]
+    tenant_id = connection_data["tenantId"]
+    client_id = connection_data["clientId"]
+    client_secret = connection_data["clientSecret"]
 
-        try:
-            logging.error("Getting token for tenant %s and client id %s..." % (tenant_id, client_id))
-            token_response = adal.acquire_token_with_client_credentials(
-                "https://login.microsoftonline.com/%s" % tenant_id,
-                client_id,
-                client_secret)
+    try:
+        logging.error("Getting token for tenant %s and client id %s..." % (tenant_id, client_id))
+        token_response = adal.acquire_token_with_client_credentials(
+            "https://login.microsoftonline.com/%s" % tenant_id,
+            client_id,
+            client_secret)
 
-            logging.info("Access token acquired")
-            response = await aiohttp.patch("http://localhost:8080/api/connections/%s" % connection_id,
-                                           data={"verified": "true"})
-        except Exception as ex:
-            logging.error("Error getting token: %s" % ex)
-            response = await aiohttp.patch("http://localhost:8080/api/connections/%s" % connection_id,
-                                           data={"verified": "false", "verificationError": ex})
+        logging.info("Access token acquired")
+        response = await aiohttp.patch("%s/connections/%s" % (api, connection_id),
+                                       data={"verified": "true"})
+    except Exception as ex:
+        logging.error("Error getting token: %s" % ex)
+        response = await aiohttp.patch("%s/connections/%s" % (api, connection_id),
+                                       data={"verified": "false", "verificationError": ex})
 
     return web.Response(body=b"{}")
 
 
+def register_callbacks():
+    data = {"pattern": "/api/connections/:id/verify", "method": "POST", "url": "http://localhost:5000/verify"}
+    requests.post(api + "/callbacks/verify-connection", data=data)
+
+    data = {"pattern": "/api/connections", "method": "POST", "url": "http://localhost:5000/connections"}
+    requests.post(api + "/callbacks/new-connection", data=data)
+
+
 if __name__ == "__main__":
+
+    register_callbacks()
+
     app = web.Application()
     app.router.add_route("POST", "/verify", test_connection)
     app.router.add_route("POST", "/connections", create_collection)
